9c852fc667566433442248781dd85cabe198b740
Richard Jarram <rjarram@me.com>
2019-10-04 10:33:43 -0400

    added binary hashes

--- average-work.js
+++ average-work.js
@@ -4,17 +4,19 @@ const blockchain = new Blockchain();
 
 blockchain.addBlock({ data: 'initial' });
 
+console.log('first block', blockchain.chain[blockchain.chain.length-1]);
+
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
 
 for (let i=0; i < 10000; i++) {
-  prevTimestamp = blockchain.chain[blockchain.lentgth-1].timestamp;
+  prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
 
   blockchain.addBlock({ data: `block ${i}`});
-  nextBlock = blockchain.chain[blockchain.length-1];
+  nextBlock = blockchain.chain[blockchain.chain.length-1];
 
-  nextTimestamp =nextBlock.timestamp;
+  nextTimestamp = nextBlock.timestamp;
   timeDiff = nextTimestamp - prevTimestamp;
   times.push(timeDiff);
 
--- block.test.js
+++ block.test.js
@@ -1,6 +1,7 @@
+const hexToBinary = require('hex-to-binary');
+const cryptoHash = require('./crypto-hash');
 const Block= require('./block');
 const { GENESIS_DATA, MINE_RATE } = require('./config');
-const cryptoHash = require('./crypto-hash');
 
 describe('Block', () => {
   const timestamp = 2000;
@@ -84,7 +85,7 @@ describe('Block', () => {
     })
 
     it('sets a `hash` that matches the difficulty criteria', () => {
-      expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+      expect(hexToBinary(minedBlock.hash).substring(0, minedBlock.difficulty))
         .toEqual('0'.repeat(minedBlock.difficulty));
     });
 
