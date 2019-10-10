d25b0b6299b31fa74ad03e3af1a97e5967c22025
Richard Jarram <rjarram@me.com>
2019-10-10 10:44:23 -0400

    set up public private keys using elliptic cryptography

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ const INITIAL_DIFFICULTY = 3;
 
 const MINE_RATE = 1000;
 
+const STARTING_BALANCE = 1000;
+
 const GENESIS_DATA = {
   timestamp:1,
   lastHash:'-----',
@@ -10,4 +12,4 @@ const GENESIS_DATA = {
   difficulty:INITIAL_DIFFICULTY
 };
 
-module.exports = { GENESIS_DATA, MINE_RATE };
+module.exports = { GENESIS_DATA, MINE_RATE, STARTING_BALANCE };
---
+++ wallet/index.test.js
@@ -0,0 +1,43 @@
+const Wallet = require('./index');
+const { verifySignature } = require('../util');
+
+describe('Wallet', () => {
+  let wallet;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+  })
+
+
+  it('has a balance', () => {
+    expect(wallet).toHaveProperty('balance');
+  });
+
+  it('has a public-key', () => {
+    expect(wallet).toHaveProperty('publicKey');
+  });
+
+  describe('signing data', () => {
+    const data = 'foo-bar';
+
+    it('verifies the signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data)
+        })
+      ).toBe(true);
+    });
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data)
+        })
+      ).toBe(false);
+    });
+  });
+});
