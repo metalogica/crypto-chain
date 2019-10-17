1e83605e4866eababacb0b3fd1ad0d852214de92
Richard Jarram <rjarram@me.com>
2019-10-17 17:56:51 +0200

    built tests for transaction wallet creation

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const Transaction = require('./transaction');
 const { verifySignature } = require('../util');
 
 describe('Wallet', () => {
@@ -38,6 +39,37 @@ describe('Wallet', () => {
           signature: new Wallet().sign(data)
         })
       ).toBe(false);
+    });
+  });
+
+  describe('createTransaction()', () => {
+    describe('and the amount exceeds the balance', () => {
+      it('throws and error', () => {
+        expect(() => wallet.createTransaction({ amount: 999999999999, recipient: 'foo-recipient' }))
+          .toThrow('Amount exceeds balance')
+      });
+    });
+
+    describe('and the amount is valid', () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        receipient = 'foo-recipient';
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      it('creates an instance of `Transaction`', () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it('matches the transaction input with the wallet', () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it('outputs the amount to the recipient', () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
