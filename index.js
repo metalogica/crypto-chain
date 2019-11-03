2edff8b7766eeaf13585518947dd53ea5b1efb3a
Richard Jarram <rjarram@me.com>
2019-11-03 17:56:56 -0500

    built post req for transact

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const wallet = new Wallet();
+const transactionPool = new TransactionPool();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -28,6 +32,18 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  const transaction = wallet.createTransaction({ recipient, amount });
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool', transactionPool);
+
+  res.json({ transaction });
 });
 
 const syncChains = () => {
