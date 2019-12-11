c48940804eb46bafc097dabb237d3112f8f66688
richard jarram <rjarram@me.com>
2019-12-11 17:34:46 -0500

    built clear transaction pool method

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -5,6 +5,22 @@ class TransactionPool {
     this.transactionMap = {};
   }
 
+  clear() {
+    this.transactionMap = {};
+  }
+
+  clearBlockchainTransactions({ chain }) {
+    for (let i=1; i<chain.length; i++) {
+      const block = chain[i];
+
+      for (let transaction of block.data) {
+        if (this.transactionMap[transaction.id]) {
+          delete this.transactionMap[transaction.id];
+        }
+      }
+    }
+  }
+
   setTransaction(transaction) {
     this.transactionMap[transaction.id] = transaction;
   }
