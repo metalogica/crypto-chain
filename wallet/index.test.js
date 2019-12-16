26eb4994059a6b5eb8b8b9b3f474c173bdc06907
richard jarram <rjarram@me.com>
2019-12-16 16:53:37 -0500

    created wallet balance function

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -143,5 +143,62 @@ describe('Wallet', () => {
         );
       });
     });
+
+    describe('and the wallet has made a transaction', ()=> {
+      let transaction;
+
+      beforeEach(() => {
+        recentTransaction = wallet.createTransaction({
+          recipient:'foo-address',
+          amount: 30
+        });
+
+        blockchain.addBlock({ data: [recentTransaction] })
+      });
+
+      it('returns the output amount of the recent transaction', () => {
+        expect(
+          Wallet.calculateBalance({
+            chain: blockchain.chain,
+            address: wallet.publicKey
+          })
+        ).toEqual(recentTransaction.outputMap[wallet.publicKey]);
+      });
+
+      describe('and there are outputs next to and after the recent transaction', () => {
+        let sameBlockTransaction, nextBlockTransaction;
+
+        beforeEach(() => {
+          recentTransaction = wallet.createTransaction({
+            recipient: 'foo-foo-address',
+            amount: 50
+          });
+
+          sameBlockTransaction = Transaction.rewardTransaction({ minerWallet: wallet });
+
+          blockchain.addBlock({ data: [recentTransaction, sameBlockTransaction] });
+
+          nextBlockTransaction = new Wallet().createTransaction({
+            recipient: wallet.publicKey,
+            amount: 75
+          });
+
+          blockchain.addBlock({ data: [nextBlockTransaction] })
+        })
+
+        it('includes the output amounts in the returned balance', () => {
+          expect(
+            Wallet.calculateBalance({
+              chain: blockchain.chain,
+              address: wallet.publicKey
+            })
+          ).toEqual(
+            recentTransaction.outputMap[wallet.publicKey] +
+            sameBlockTransaction.outputMap[wallet.publicKey] +
+            nextBlockTransaction.outputMap[wallet.publicKey]
+          );
+        });
+      });
+    });
   });
 });
