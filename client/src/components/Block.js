8eecf7cfb57c56cf0bcb315315ccfb65a79dcda9
richard jarram <rjarram@me.com>
2020-02-03 17:55:38 -0500

    debug on the crypto chain component

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 class Block extends Component {
   state = { displayTransaction: false };
@@ -11,17 +13,53 @@ class Block extends Component {
   get displayTransaction() {
     const { data } = this.props.block;
 
-    const transactionalData = JSON.stringify(data);
+    const stringifiedData = JSON.stringify(data);
 
-    const dataDisplay = stringifiedData.length > 35 ? `${stringifiedData.substring(0, 35)}...` : stringifiedData;
+    const dataDisplay = stringifiedData.length > 35 ?
+      `${stringifiedData.substring(0, 35)}...` :
+      stringifiedData;
 
-    return <div>Data: {dataDisplay}</div>;
+    if (this.state.displayTransaction) {
+      return (
+        <div>
+          {
+            data.map(transaction => (
+              <div key={transaction.id}>
+                <hr />
+                <Transaction transaction={transaction} />
+              </div>
+            ))
+          }
+        <br/>
+          <Button
+            bsStyle="danger"
+            bsSize="small"
+            onClick={this.toggleTransaction}
+          >
+            Show Less
+          </Button>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <div>Data: {dataDisplay}</div>
+        <Button
+          bsStyle="danger"
+          bsSize="small"
+          onClick={this.toggleTransaction}
+        >
+          Show More
+        </Button>
+      </div>
+    );
   }
 
   render() {
     const { timestamp, hash } = this.props.block;
+
     const hashDisplay = `${hash.substring(0, 15)}...`;
-    const stringifiedData = JSON.stringify(data);
 
     return(
       <div className='Block'>
