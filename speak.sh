066aed476b22c2ecc668f87a05ae31fbd41d3da1
richard jarram <rjarram@me.com>
2020-03-20 18:54:33 -0400

    testing

--- speak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-message=$1
-cd $CODE/crypto-chain
-echo 'test-' >> test.txt
-git add .
-git commit -m "${message}"
-git push
-echo "successfully pushed to git with following message: ${message}"
